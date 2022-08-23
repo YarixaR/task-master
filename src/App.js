003cba7b8ad03634d3e794b06fdad0107bda8bd7
Remberto Villar <rembertovillar@gmail.com>
2022-08-22 20:00:12 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
