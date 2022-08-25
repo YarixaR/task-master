3b5a19b60668d50cbe7fc8c0f5a14d0316ef5a9b
Remberto Villar <rembertovillar@gmail.com>
2022-08-25 09:42:21 -0400

    fixed form

--- src/App.js
+++ src/App.js
@@ -5,43 +5,57 @@ import Footer from './components/Footer';
 import {useEffect, useState} from 'react';
 // import { GlobalStyles } from '@mui/styled-engine';
 
-
 function App() {
  
-
   const [ tasks, setTasks ] = useState([])
-
   const [ searchString, setSearchString ] = useState ('')
 
 
  const changeSearchString = tasks.filter(task => {
   return task.title.toLowerCase().includes(searchString.toLowerCase()) || task.description.toLowerCase().includes(searchString.toLowerCase()) || task.category.toLowerCase().includes(searchString.toLowerCase())
  })
-  
 
-  useEffect(() => {
-    fetch( "http://localhost:9292/tasks" )
+ const getTask = () => {
+  fetch( "http://localhost:9292/tasks" )
     .then( resp => resp.json() )
     .then( data => setTasks( data ) )
+ }
+
+  useEffect(() => {
+    getTask()
   }, [])
 
+  // update tasks 
+  function handleUpdateTask() {
+    // const updatedTasks = tasks.map((task) => {
+    //   if (task.id === updatedTaskObj.id) {
+    //     return updatedTaskObj
+    //   } else {
+    //     return task
+    //   }
+    // })
+    // setTasks(updatedTasks) 
+    getTask()
+  }
+
   // delete task card function
-  function deleteTask(id) {
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    setTasks(updatedTasks)
+  function deleteTask() {
+    // const updatedTasks = tasks.filter((task) => task.id !== id);
+    // setTasks(updatedTasks)
+    getTask()
   }
   
   const handleAddTask = (addedTask) => {
     setTasks([ ...tasks, addedTask])
   }
-  console.log(tasks)
   
   return (
     <div className="App">
       <Header setSearchString = { setSearchString } />
       <Home  taskList = {changeSearchString}
-      handleAddTask={handleAddTask}
-      deleteTask = {deleteTask}
+        handleAddTask={handleAddTask}
+        deleteTask = {deleteTask}
+        handleUpdateTask = {handleUpdateTask}
       />
       <Footer />
     </div>
